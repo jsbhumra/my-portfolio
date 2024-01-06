7f63b8a6899ffae4df4660380bac0bb0f73be929
jsbhumra <jagjit.s.bhumra@gmail.com>
2024-01-06 18:46:45 +0530

    Simple color edit

--- components/Android/ChatSection/index.js
+++ components/Android/ChatSection/index.js
@@ -101,7 +101,7 @@ export default function ChatSection({ curr, onShow, theme, onHide }) {
                     </div>
                 </div>
             </div>
-            <div className="mx-[4px] w-[48px] flex items-center justify-center h-[48px] my-[7px] rounded-full cursor-pointer bg-[#028069]">
+            <div className="mx-[4px] w-[48px] flex items-center justify-center h-[48px] my-[7px] rounded-full cursor-pointer bg-[#00a683]">
                 <div role="button" className="w-[45px] h-[45px] flex items-center justify-center text-[#ffffff] cursor-pointer rounded-full">
                     <svg viewBox="0 0 24 24" height="24" width="24" preserveAspectRatio="xMidYMid meet" className="" version="1.1" x="0px" y="0px" enableBackground="new 0 0 24 24"><title>ptt</title><path fill="currentColor" d="M11.999,14.942c2.001,0,3.531-1.53,3.531-3.531V4.35c0-2.001-1.53-3.531-3.531-3.531 S8.469,2.35,8.469,4.35v7.061C8.469,13.412,9.999,14.942,11.999,14.942z M18.237,11.412c0,3.531-2.942,6.002-6.237,6.002 s-6.237-2.471-6.237-6.002H3.761c0,4.001,3.178,7.297,7.061,7.885v3.884h2.354v-3.884c3.884-0.588,7.061-3.884,7.061-7.885 L18.237,11.412z"></path></svg>
                 </div>
