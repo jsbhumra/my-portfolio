d40541bb02e9af09aa17405f0ae897fa10012a6d
jsbhumra <jagjit.s.bhumra@gmail.com>
2024-01-22 14:18:40 +0530

    Further iOS edits
     - Made ChatGroup line clamp to 2 for iOS
     - Added some translucent elements (top and bottom bars)

--- components/IOS/ChatGroup/page.js
+++ components/IOS/ChatGroup/page.js
@@ -26,20 +26,20 @@ export default function ChatGroup({ name, datetime, text, img, mute, onClick, se
                     <Image src={`/group-icons/${img}`} width={30} height={30} className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2" style={{ filter: theme!="dark" && 'brightness(0) invert(1)' }} />
                 </div>
             </div>
-            <div className="flex flex-col justify-center overflow-hidden h-full w-full border-b border-solid border-[#e9edef] dark:border-[#222d34] pr-[15px]">
+            <div className="flex flex-col justify-start pt-[10px] overflow-hidden h-full w-full border-b border-solid border-[#e9edef] dark:border-[#222d34] pr-[15px]">
                 <div className="w-full flex items-center text-left justify-between overflow-hidden whitespace-nowrap">
-                    <div className="text-left overflow-hidden leading-[21px] text-[16px] font-normal overflow-hidden whitespace-nowrap text-ellipsis text-[#111b21] dark:text-[#e9edef]">
+                    <div className="text-left overflow-hidden leading-[21px] text-[16px] font-[550] overflow-hidden whitespace-nowrap text-ellipsis text-[#111b21] dark:text-[#e9edef]">
                     {name}
                     </div>
-                    <div className="mt-[3px] ml-[3px] leading-[14px] text-[12px] max-w-full overflow-hidden whitespace-nowrap text-ellipsis text-[#667781] dark:text-[#8696a0]">
+                    <div className="-mt-[1px] ml-[3px] leading-[14px] text-[12px] max-w-full overflow-hidden whitespace-nowrap text-ellipsis text-[#667781] dark:text-[#8696a0]">
                         {datetime}
                     </div>
                 </div>
-                <div className="w-full mt-[6px] overflow-hidden flex justify-between items-center min-h-[20px] text-[13px] leading-[20px] text-[#667781] dark:text-[#8696a0]">
-                    <div className="flex text-left overflow-hidden leading-[20px] text-[14px] whitespace-nowrap font-normal text-[#8596a1] dark:text-[#8696a0]">
-                    <div className="overflow-hidden text-ellipsis whitespace-nowrap">{text}</div>
+                <div className="w-full mt-[0px] overflow-hidden flex justify-between items-start min-h-[20px] text-[13px] leading-[20px] text-[#667781] dark:text-[#8696a0]">
+                    <div className="flex text-left leading-[20px] text-[14px] overflow-hidden font-normal text-[#8596a1] dark:text-[#8696a0]">
+                    <div className="overflow-hidden text-ellipsis line-clamp-2">{text}</div>
                     </div>
-                    <div className="flex flex-row ml-[6px] max-w-full text-[12px] leading-[20px] text-[#8596a1] dark:text-[#8696a0]">
+                    <div className="flex flex-row justify-end ml-[6px] max-w-full text-[12px] leading-[20px] text-[#8596a1] dark:text-[#8696a0]">
                         {mute && <span><svg viewBox="0 0 16 18" height="18" width="16" preserveAspectRatio="xMidYMid meet" className="" version="1.1" x="0px" y="0px" enableBackground="new 0 0 16 18"><title>muted</title><path fill="currentColor" d="M11.52,9.206c0-1.4-0.778-2.567-1.944-3.111v1.711L11.52,9.75 C11.52,9.517,11.52,9.362,11.52,9.206z M13.465,9.206c0,0.7-0.156,1.4-0.389,2.022l1.167,1.167c0.544-0.933,0.778-2.1,0.778-3.267 c0-3.344-2.333-6.144-5.444-6.844v1.633C11.832,4.695,13.465,6.717,13.465,9.206z M2.032,2.206L1.02,3.217l3.656,3.656H1.02v4.667 h3.111l3.889,3.889v-5.211l3.344,3.344c-0.544,0.389-1.089,0.7-1.789,0.933v1.633c1.089-0.233,2.022-0.7,2.878-1.4l1.556,1.556 l1.011-1.011l-7-7L2.032,2.206z M8.02,2.984L6.387,4.617L8.02,6.25V2.984z"></path></svg></span>}
                     </div>
                 </div>
